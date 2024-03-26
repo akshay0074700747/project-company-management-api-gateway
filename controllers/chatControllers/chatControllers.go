22a144f440e4b9ab5eeb6f37b2e1e525eebb532d
akshay <ask029849@gmail.com>
2024-03-26 15:04:13 +0530

    deploying to k8s

--- controllers/chatControllers/chatControllers.go
+++ controllers/chatControllers/chatControllers.go
@@ -1,10 +1,13 @@
 package chatcontrollers
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func chatConteroller(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("======here=======websocket========")
 
 	http.Redirect(w, r, "http://chat-service.default.svc.cluster.local:50006/ws", http.StatusFound)
 }
